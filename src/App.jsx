ca62a03e69a0041d7afccbfe55b62995260f0308
Boikhutso Mokoka <boikhuso.m@gmail.com>
2024-05-31 09:18:00 +0200

    checking the folder

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,10 @@
-
 function App() {
-
   return (
     <>
-     
+      <h1>Wellcome Boikhutso</h1>
+      <h2>Wellcome to my page</h2>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
