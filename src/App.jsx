a5483df074c94578ed7330276dd1319b3f179fed
Boikhutso Mokoka <boikhuso.m@gmail.com>
2024-05-31 14:24:51 +0200

    Form template

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Header from "../component/Header";
+import Meme from "../component/Meme";
 
 /* Build the Header component */
 export default function App() {
   return (
     <div>
       <Header />
+      <Meme />
     </div>
   );
 }
